6de4b67666a62179857183a66dd367d7fa157566
Tarivs <zyt.larive@gmail.com>
2016-10-23 16:37:42 +0800

    Fix ImportError: No module named qiniu

--- examples/list_log.py
+++ examples/list_log.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '..'))
 
 from qiniu import Auth, BucketManager
 
